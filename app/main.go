0b701ea1536256b7423fe6e3d06db32e4b1a469d
Pedro Santos <donbonifacio@gmail.com>
2025-03-05 11:27:47 +0000

    [any message]

--- app/main.go
+++ app/main.go
@@ -11,36 +11,94 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+type CommandArgs struct {
+	Cmd  *Command
+	Cmds map[string]Command
+	Raw  string
+	Args []string
+}
+
+type Command struct {
+	Name string
+	Fn   func(input *CommandArgs) error
+}
+
+var commands = map[string]Command{
+	"echo": {
+		Name: "echo",
+		Fn:   echo,
+	},
+	"exit": {
+		Name: "exit",
+		Fn:   exit,
+	},
+	"type": {
+		Name: "type",
+		Fn:   typeFn,
+	},
+	"": {
+		Name: "unknown command",
+		Fn:   unknownCommand,
+	},
+}
+
+func echo(input *CommandArgs) error {
+	fmt.Fprintln(os.Stdout, strings.Replace(input.Raw, input.Args[0]+" ", "", -1))
+	return nil
+}
+
+func typeFn(input *CommandArgs) error {
+	target := input.Args[1]
+	if _, ok := input.Cmds[target]; ok {
+		fmt.Fprintf(os.Stdout, "%v is a shell builtin\n", target)
+		return nil
+	}
+	fmt.Fprintf(os.Stdout, "%v: not found\n", target)
+	return nil
+}
+
+func exit(input *CommandArgs) error {
+	if len(input.Args) > 1 {
+		num, err := strconv.Atoi(input.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(num)
+	}
+	os.Exit(0)
+	return nil
+}
+
+func unknownCommand(input *CommandArgs) error {
+	fmt.Fprintf(os.Stdout, "%v: command not found\n", input.Raw)
+	return nil
+}
+
 func main() {
 
 	for true {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		raw, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
-		input = strings.TrimSpace(input)
-		args := strings.Split(input, " ")
-		if len(input) >= 0 {
-			if args[0] == "exit" {
-				if len(args) > 1 {
-					num, err := strconv.Atoi(args[1])
-					if err != nil {
-						panic(err)
-					}
-					os.Exit(num)
-				}
-				os.Exit(0)
-			} else if args[0] == "echo" {
-				fmt.Fprintln(os.Stdout, strings.Replace(input, args[0]+" ", "", -1))
-			} else {
-				fmt.Fprintf(os.Stdout, "%v: command not found\n", input)
+		raw = strings.TrimSpace(raw)
+		input := CommandArgs{
+			Cmds: commands,
+			Raw:  raw,
+			Args: strings.Split(raw, " "),
+		}
+
+		cmd := commands[input.Args[0]]
+		if cmd.Fn != nil {
+			input.Cmd = &cmd
+			if err := cmd.Fn(&input); err != nil {
+				panic(err)
 			}
 		} else {
-			fmt.Fprintf(os.Stdout, "%v: command not found\n", input)
-
+			unknownCommand(&input)
 		}
 	}
 
